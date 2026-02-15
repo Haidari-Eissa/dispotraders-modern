45c4a1a9eb7d1f48979647ea19089940b82c393f
Haidari-Eissa <haidarieissa@gmial.com>
2026-02-15 12:29:12 +0100

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class", // <--- CHANGED THIS (No brackets [])
+  darkMode: "class", // <--- LOOK HERE: No brackets []
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
